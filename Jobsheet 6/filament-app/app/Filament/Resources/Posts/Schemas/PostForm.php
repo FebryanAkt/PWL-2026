1c23da4a306f1db2f6419440f1e31b71ad125b42
FebryanAkt <febryan08022006@gmail.com>
2026-03-30 09:21:26 +0700

    Custom Layout Form

--- Jobsheet 6/filament-app/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ Jobsheet 6/filament-app/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Posts\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Group;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\ColorPicker;
@@ -11,6 +13,7 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Support\Icons\Heroicon;
 
 class PostForm
 {
@@ -18,26 +21,59 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                ->required()
-                ->minLength(5),
-                TextInput::make('slug')
-                ->required()
-                ->unique(ignoreRecord: true),
-                
-                Select::make("category_id")
-                ->relationship("category", "name")
-                ->preload()
-                ->searchable(),
-                ColorPicker::make("color"),
-                MarkdownEditor::make("body"),
-                FileUpload::make("image")
-                    ->disk('public')
-                    ->directory('posts'),
-                TagsInput::make("tags"),
-                Checkbox::make("published"),
-                DateTimePicker::make("published_at"),
 
-            ]);
+                // 🔹 KIRI (2/3)
+                Section::make("Post Details")
+                    ->description("Fill in the details of the post")
+                    ->icon(Heroicon::OutlinedDocumentText)
+                    ->schema([
+
+                        Group::make([
+                            TextInput::make("title")
+                                ->required()
+                                ->minLength(5),
+
+                            TextInput::make("slug")
+                                ->required()
+                                ->unique(ignoreRecord: true),
+
+                            Select::make("category_id")
+                                ->relationship("category", "name")
+                                ->preload()
+                                ->searchable(),
+
+                            ColorPicker::make("color"),
+                        ])->columns(2),
+
+                        MarkdownEditor::make("content")
+                            ->columnSpanFull(),
+
+                    ])
+                    ->columnSpan(2),
+
+                // 🔹 KANAN (1/3)
+                Group::make([
+
+                    Section::make("Image Upload")
+                        ->icon(Heroicon::OutlinedPhoto)
+                        ->schema([
+                            FileUpload::make("image")
+                                ->disk("public")
+                                ->directory("posts"),
+                        ]),
+
+                    Section::make("Meta Information")
+                        ->icon(Heroicon::OutlinedAdjustmentsHorizontal)
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                            DateTimePicker::make("published_at"),
+                        ]),
+
+                ])
+                ->columnSpan(1),
+
+            ])
+            ->columns(3);
     }
 }
