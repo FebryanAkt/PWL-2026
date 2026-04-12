900d3f3faeec817efd7ae88923c1343af12de126
FebryanAkt <febryan08022006@gmail.com>
2026-04-12 08:55:26 +0700

    praktikum Implementasi Wizard Form

---
+++ Jobsheet 7/filament-app/app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Schemas\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Checkbox;   
+use Filament\Actions\Action;
+
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Wizard::make([
+                    Step::make('Product Info')
+                        ->icon('heroicon-o-information-circle')
+
+                        ->description('Isi informasi produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('sku')
+                                    ->required(),
+                            ])->columns(2),
+
+                            MarkdownEditor::make('description'),
+                        ]),
+                        Step::make('Product Price and Stock')
+                            ->icon('heroicon-o-currency-dollar')
+                            
+
+                        ->description('Isi Harga Produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('price')
+                                    ->required()
+                                    ->numeric()
+                                    ->minValue(1),
+                                TextInput::make('stock')
+                                    ->required(),
+                            ])->columns(2),
+
+                            MarkdownEditor::make('description'),
+                        ]),
+                        Step::make('Media and status')
+                            ->icon('heroicon-o-photo')
+                        ->description('Isi Gambar Produk')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->image()
+                                ->required()
+                                ->disk('public')
+                                ->directory('products')
+                                ->visibility('public'),
+
+                            Checkbox::make('is_active'),
+                            Checkbox::make('is_featured'),
+                        ]),
+                        
+                    ])
+                    ->columnSpanFull()
+                    ->submitAction(
+                        Action::make('save')
+                            ->label('Save Product')
+                            ->button()
+                            ->color('primary')
+                            ->submit('save')
+                    )
+            ]);
+    }
+}
